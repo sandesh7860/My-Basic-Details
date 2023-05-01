0ce35e8bcce9b1335769ff07babd87d5a8816248
San Dy <sandy@Sans-MacBook-Air.local>
2023-05-01 12:04:19 +0545

    first commit

---
+++ Mybasicdetails.java
@@ -0,0 +1,30 @@
+public class Mybasicdetails {
+    public static void main(String[] args){
+        int age = 20;
+        int studentID = 2357608;
+        String name = "Sandesh Shahi";
+        String address = "Baluwatar Kathmandu";
+        String currentEdu = "Herald College Kathmandu";
+        String batch = "First Semester";
+        String subject = "BCS";
+        String skills = "Java, HTML, CSS, Javascript, Python ";
+        boolean isMarried = false;
+        double height = 5.7;
+        char gender = 'M';
+        System.out.println("My name is:" +name);
+        System.out.println("My age is:" + age);
+        System.out.println("My address is:" + address);
+        System.out.println("I am married:" + isMarried);
+        System.out.println("My height is:" + height);
+        System.out.println("Gender:" + gender);
+        System.out.println("Current Education:" + currentEdu);
+        System.out.println("Subject:" + subject);
+        System.out.println("Current Semester:" + batch);
+        System.out.println("Student ID:" + studentID);
+        System.out.println("Skills:" + skills);
+
+
+
+
+    }
+}
