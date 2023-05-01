2ec0233afc54e023b09fadc7b0c868d5acf5526c
San Dy <sandy@Sans-MacBook-Air.local>
2023-05-01 14:22:55 +0545

    first commit

---
+++ areaofcircle/Final.java
@@ -0,0 +1,11 @@
+package areaofcircle;
+
+public class Final {
+    public static void main(String[] args){
+
+        final double PI = 6.14;
+        double radius = 8;
+        double areaOf_Circle = PI * radius * radius;
+        System.out.println("The are of circle is:" + areaOf_Circle);
+    }
+}
